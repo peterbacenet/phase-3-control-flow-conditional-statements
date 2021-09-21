480c77e2a40fef93289a15c7cd1a814ad8525145
Peter Bacenet <peterbacenet@icloud.com>
2021-09-21 09:09:41 -0400

    commit tree 06d9db7fc1c36da15306199498d773efd33d2425

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,53 @@
+
 def admin_login(username, password)
-  # your code here
+#regular if/else
+#note the parentheses for ||
+if (username == "admin" || username == "ADMIN") && password == "12345"
+  "Access granted"
+else 
+  "Access denied"
 end
+end
+
 
 def hows_the_weather(temperature)
-  # your code here
+#if/elseif
+response = if temperature >= 85
+  "It's too dang hot out there!"
+elsif temperature >= 40 && temperature <= 60
+  "It's a little chilly out there!"
+elsif temperature < 40
+  "It's brisk out there!"
+else
+  "It's perfect out there!"
 end
+  end
 
 def fizzbuzz(num)
-  # your code here
-end
+#if/elseif
+if (num % 3 == 0 && num % 5 == 0)
+  "FizzBuzz"
+  elsif num % 3 == 0 
+    "Fizz"
+  elsif num % 5 == 0 
+    "Buzz"
+  else
+    num
+  end
+  end
 
 def calculator(operation, num1, num2)
-  # your code here
+#case/when
+case operation
+when "+"
+  num1 + num2
+when  "-"
+  num1 - num2
+when  "*"
+  num1 * num2
+when  "/"
+  num1 / num2
+else
+  puts "Invalid operation!"
 end
-
+end
